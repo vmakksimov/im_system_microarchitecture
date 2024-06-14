e7330d74ae595a325f0f2b864cdc3625f9fc122c
vmaksimov <viktor.maksimov@oblak.bg>
2024-06-14 16:58:33 +0300

    adding initial commit

---
+++ frontend/src/routes.jsx
@@ -0,0 +1,28 @@
+import { Home, Profile, SignIn, SignUp } from "@/pages";
+
+export const routes = [
+  {
+    name: "home",
+    path: "/home",
+    element: <Home />,
+  },
+
+  {
+    name: "Sign In",
+    path: "/sign-in",
+    element: <SignIn />,
+  },
+  {
+    name: "Sign Up",
+    path: "/sign-up",
+    element: <SignUp />,
+  },
+  {
+    name: "Docs",
+    href: "https://www.material-tailwind.com/docs/react/installation",
+    target: "_blank",
+    element: "",
+  },
+];
+
+export default routes;
---
+++ frontend/src/widgets/particals/ParticleBackground.jsx
@@ -0,0 +1,89 @@
+// ParticleBackground.js
+import React from 'react';
+import Particles from 'react-tsparticles';
+import { loadFull } from 'tsparticles';
+
+const ParticleBackground = () => {
+  const particlesInit = async (main) => {
+    await loadFull(main);
+  };
+
+  const particlesOptions = {
+    fpsLimit: 280,
+    interactivity: {
+      events: {
+        onClick: {
+          enable: true,
+          mode: "push",
+        },
+        onHover: {
+          enable: true,
+          mode: "repulse",
+        },
+        resize: true,
+      },
+      modes: {
+        bubble: {
+          distance: 400,
+          duration: 2,
+          opacity: 0.8,
+          size: 40,
+        },
+        push: {
+          quantity: 4,
+        },
+        repulse: {
+          distance: 200,
+          duration: 0.4,
+        },
+      },
+    },
+    particles: {
+      color: {
+        value: "#ffffff",
+      },
+      links: {
+        color: "#ffffff",
+        distance: 150,
+        enable: true,
+        opacity: 0.7,
+        width: 1.5,
+      },
+      collisions: {
+        enable: true,
+      },
+      move: {
+        direction: "none",
+        enable: true,
+        outMode: "bounce",
+        random: false,
+        speed: 3,
+        straight: false,
+      },
+      number: {
+        density: {
+          enable: true,
+          area: 800,
+        },
+        value: 80,
+      },
+      opacity: {
+        value: 0.5,
+      },
+      shape: {
+        type: "circle",
+      },
+      size: {
+        random: true,
+        value: 5,
+      },
+    },
+    detectRetina: true,
+  };
+
+  return (
+    <Particles id="tsparticles" init={particlesInit} options={particlesOptions} />
+  );
+};
+
+export default ParticleBackground;
